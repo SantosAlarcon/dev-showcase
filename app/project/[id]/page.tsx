ff1b745c0a39c59b2411bbe3d93ad6cd2e2072c4
Santos Alarcón <santosalarcon86@gmail.com>
2025-05-16 13:10:34 +0200

    wip: work on project page

--- app/project/[id]/page.tsx
+++ app/project/[id]/page.tsx
@@ -1,6 +1,10 @@
 import ProjectNotFound from "@/components/layout/ProjectNotFound";
+import { DeveloperInfo, Project } from "@/types/types";
+import { getDeveloperInfo } from "@/utils/getDeveloperInfo";
 import { getProjectInfo } from "@/utils/getProjectInfo";
-import { Box } from "@mui/joy";
+import { Avatar, Box, Chip, Link } from "@mui/joy";
+import { MarkdownAsync } from "react-markdown";
+import remarkGfm from "remark-gfm";
 
 export async function generateMetadata(props: {
 	params: Promise<{ params: { id: string } }>;
@@ -16,16 +20,62 @@ export async function generateMetadata(props: {
 	};
 }
 
-export default async function ProjectProfile(props: {params: Promise<{ params: { id: string } }>}) {
+export default async function ProjectProfile(props: {
+	params: Promise<{ params: { id: string } }>;
+}) {
 	const params = await props.params;
 	// @ts-ignore
 	const { id } = params;
-	const project = getProjectInfo(id);
+	const project: Project | undefined = getProjectInfo(id);
+	const developer: DeveloperInfo | undefined = getDeveloperInfo(
+		// @ts-ignore
+		project?.userId,
+	);
 
 	if (!project) return <ProjectNotFound />;
 	return (
-		<Box p={8}>
-			<h1>Project</h1>
+		<Box
+			p={8}
+			sx={{
+				display: "flex",
+				flexDirection: "column",
+				gap: 2,
+				alignItems: "center",
+			}}
+		>
+			<h1>{project.title}</h1>
+			<Box sx={{ display: "flex", gap: 1, alignItems: "center" }}>
+				<Avatar
+					src={developer.avatar}
+					alt={developer.name + developer.surname}
+				/>{" "}
+				<Link href={`/developer/${project.userId}`}>
+					{developer.name} {developer.surname}
+				</Link>{" "}
+				- <b>Published on</b>{" "}
+				{new Date(project.publishedDate).toLocaleDateString("en-US", {
+					year: "numeric",
+					month: "long",
+					day: "numeric",
+				})}
+			</Box>
+			<Box sx={{ alignSelf: "start" }}>
+				<MarkdownAsync remarkPlugins={[remarkGfm]}>
+					{project.story}
+				</MarkdownAsync>
+			</Box>
+			<Box sx={{alignSelf: "start"}}>
+				<h2>Technologies Used</h2>
+				{project.technologies.map((tech, index) => (
+					<Chip
+						key={index}
+						size="md"
+						sx={{ borderRadius: "10rem", mx: 0.5 }}
+					>
+						<b>{tech}</b>
+					</Chip>
+				))}
+			</Box>
 		</Box>
-	)
-};
+	);
+}

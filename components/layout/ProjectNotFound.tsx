984bf726b0dc6042eecb6a2c97e2949fbe9c020e
Santos Alarcón <santosalarcon86@gmail.com>
2025-05-21 11:48:40 +0200

    chore: removed radix-ui

--- components/layout/ProjectNotFound.tsx
+++ components/layout/ProjectNotFound.tsx
@@ -7,21 +7,33 @@ const UserNotFound = () => {
     return (
         <Box
             component="main"
-            px={4}			
-			py={{ xs: 4, sm: 24 }}
+            px={4}
+            py={{ xs: 4, sm: 24 }}
             sx={{
                 display: "flex",
                 flexDirection: "column",
                 alignItems: "center",
                 justifyContent: "center",
-		}}
-	>
+            }}
+        >
             <h1>This project cannot be found</h1>
-            <Typography level="body-md" display="flex" alignItems={"center"} flexDirection={"column"} gap={2} sx={{textDecoration: "none"}}>
-                This project may have been deleted or the link may be broken.
-                But don't worry, you can discover new projects by clicking
-                this button below
-                <Button component={Link} href="/projects" sx={{width: "fit-content"}} aria-label="Discover new projects">Discover new projects</Button>
+            <Typography
+                level="body-md"
+                display="flex"
+                alignItems={"center"}
+                flexDirection={"column"}
+                gap={2}
+                sx={{ textDecoration: "none" }}
+            >
+                This project may have been deleted or the link may be broken. But don't worry, you can discover new projects by clicking this button below.
+                <Button
+                    component={Link}
+                    href="/projects"
+                    sx={{ width: "fit-content" }}
+                    aria-label="Discover new projects"
+                >
+                    Discover new projects
+                </Button>
             </Typography>
         </Box>
     );
--- components/layout/UserNotFound.tsx
+++ components/layout/UserNotFound.tsx
@@ -7,21 +7,33 @@ const UserNotFound = () => {
     return (
         <Box
             component="main"
-            px={4}			
-			py={{ xs: 4, sm: 24 }}
+            px={4}
+            py={{ xs: 4, sm: 24 }}
             sx={{
                 display: "flex",
                 flexDirection: "column",
                 alignItems: "center",
                 justifyContent: "center",
-		}}
-	>
+            }}
+        >
             <h1>This user cannot be found</h1>
-            <Typography level="body-md" display="flex" alignItems={"center"} flexDirection={"column"} gap={2} sx={{textDecoration: "none"}}>
-                This user may have been deleted or the link may be broken.
-                But don't worry, you can discover new developers by clicking
-                this button below
-                <Button component={Link} href="/discover" sx={{width: "fit-content"}} aria-label="Discover new developers">Discover new developers</Button>
+            <Typography
+                level="body-md"
+                display="flex"
+                alignItems={"center"}
+                flexDirection={"column"}
+                gap={2}
+                sx={{ textDecoration: "none" }}
+            >
+                This user may have been deleted or the link may be broken. But don't worry, you can discover new developers by clicking this button below.
+                <Button
+                    component={Link}
+                    href="/discover"
+                    sx={{ width: "fit-content" }}
+                    aria-label="Discover new developers"
+                >
+                    Discover new developers
+                </Button>
             </Typography>
         </Box>
     );

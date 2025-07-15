cd8bcda4a70068fd98425eceaebabff3f7fa3ea2
Santos Alarcon Asensio <santos@localhost.localdomain>
2025-07-15 20:01:07 +0200

    wip: login page

---
+++ app/login/layout.tsx
@@ -0,0 +1,18 @@
+import MinimalLayout from "@/components/layout/MinimalLayout";
+
+export async function generateMetadata() {
+	return {
+		title: "Login - Talent Showcase",
+		description: "Login page for Talent Showcase",
+	};
+}
+
+const LoginLayout = ({ children }: { children: React.ReactNode }) => {
+	return (
+		<MinimalLayout>
+			{children}
+		</MinimalLayout>
+	)
+}
+
+export default LoginLayout;
---
+++ components/layout/MinimalLayout.tsx
@@ -0,0 +1,24 @@
+import Providers from "@/app/providers";
+import { Figtree } from "next/font/google";
+
+const font = Figtree({
+	subsets: ["latin"],
+	display: "swap",
+	variable: "--font-figtree",
+});
+
+const MinimalLayout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <html lang="en" className={font.className}>
+            <body>
+                <Providers>
+                    <div>
+                        <main>{children}</main>
+                    </div>
+                </Providers>
+            </body>
+        </html>
+    );
+};
+
+export default MinimalLayout;

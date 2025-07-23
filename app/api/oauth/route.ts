0d6d64f8d92612df29410122e0accb789303a52a
Santos Alarcón <santosalarcon86@gmail.com>
2025-07-23 12:53:39 +0200

    fix: oauth login doesnt create any session

--- app/api/oauth/route.ts
+++ app/api/oauth/route.ts
@@ -1,12 +1,24 @@
+import { account } from "@/lib/appwrite/client";
+import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-	const { searchParams } = new URL(req.url);
+	const { searchParams } = req.nextUrl;
 	const secret = searchParams.get("secret");
 	const userId = searchParams.get("userId");
+	const cookieList = await cookies();
 
 	if (secret && userId) {
 		try {
+			const session = await account.createSession(userId, secret);
+			cookieList.set("dev_showcase_session", JSON.stringify(session), {
+				path: "/",
+				// httpOnly: true,
+				// secure: process.env.NODE_ENV === "production",
+				// sameSite: "lax",
+				// maxAge: 60 * 60 * 24 * 30,
+			});
+
 			return NextResponse.redirect(new URL("/discover", req.url));
 		} catch (error) {
 			console.error("Error during OAuth callback:", error);
